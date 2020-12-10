ce5cf726e8c255ca1687ca1c38a904f71af1ba5c
xsvanc06 <xsvanc06@stud.fit.vutbr.cz>
2020-12-10 14:02:44 +0100

    cons arguments

--- cons.py
+++ cons.py
@@ -1,9 +1,30 @@
 from kafka import KafkaConsumer
 from json import loads
+import sys,getopt
+
+broker = ['51.116.188.112:9092']
+topic = 'quickstart-events'
+full_cmd_arguments = sys.argv
+argument_list = full_cmd_arguments[1:]
+short_options = "b:t:h"
+
+try:
+    arguments, values = getopt.getopt(argument_list, short_options, "")
+except getopt.error as err:
+    print (str(err))
+    sys.exit(2)
+for current_argument, current_value in arguments:
+    if current_argument in ("-b"):
+        broker[0] = current_value
+    elif current_argument in ("-h"):
+        print ("    Usage:\n     -b <broker>   ~~~~ sets broker (default 51.116.188.112:9092)\n     -t <topic>    ~~~~ sets topic  (default quickstart-events)")
+        sys.exit(0)
+    elif current_argument in ("-t"):
+        topic = current_value
 
 consumer = KafkaConsumer(
-	'quickstart-events',
-	bootstrap_servers=['51.116.188.112:9092'],
+	topic,
+	bootstrap_servers=broker,
 	auto_offset_reset='latest',
 	enable_auto_commit=True,
 	group_id=None)
